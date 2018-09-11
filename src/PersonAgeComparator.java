c2e6289e3c39f68e94b13d7d65491e1775d32d2f
GerdtKristina <gerdt.kristina@gmail.com>
2018-09-11 18:46:02 +0200

    optimization compare() in PersonAgeComparator

--- src/PersonAgeComparator.java
+++ src/PersonAgeComparator.java
@@ -3,11 +3,6 @@ import java.util.Comparator;
 public class PersonAgeComparator implements Comparator<Person> {
     @Override
     public int compare(Person o1, Person o2) {
-        if (o1.getAge() > o2.getAge())
-            return 1;
-        else if (o1.getAge() < o2.getAge())
-            return -1;
-        else
-            return 0;
+       return Integer.compare(o1.getAge(), o2.getAge());
     }
 }
